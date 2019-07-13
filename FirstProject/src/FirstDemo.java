bae443071b1e9fb4bda36155cc6e3aca2dca42bd
wordjava <wordjava1@163.com>
2019-07-13 12:46:28 +0800

    在虚拟机中,第一次提交demo到个

--- FirstProject/src/FirstDemo.java
+++ FirstProject/src/FirstDemo.java
@@ -2,7 +2,7 @@ public class FirstDemo {
     public static void main(String[] args){
         System.out.println("aslkfdls我就死死");
         for (int i = 0; i < 10; i++) {
-
+            System.out.println("aa");
         }
     }
 }
